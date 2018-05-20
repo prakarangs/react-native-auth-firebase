28f6c5559c8243d9888724dcd7e4d6f431d88eb8
kao <kao@aklamio.com>
2018-05-20 16:27:45 +0200

    Add password form

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { TextInput, View, Text, StyleSheet } from 'react-native';
 
-const Input = ({ label, value, onChangeText }) => {
+const Input = ({
+  label,
+  value,
+  onChangeText,
+  placeholder,
+  secureTextEntry
+}) => {
   return (
     <View style={styles.containerStyle}>
       <Text style={styles.labelStyle}>{label}</Text>
       <TextInput
+        secureTextEntry={secureTextEntry}
+        placeholder={placeholder}
+        autoCorrect={false}
         style={styles.inputStyle}
         value={value}
         onChangeText={onChangeText}
